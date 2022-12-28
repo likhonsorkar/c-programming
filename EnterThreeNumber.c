31d4b8b2c7aed296c38704aca0936522557332bf
unknown <likhonsorkar02@yahoo.com>
2022-12-28 11:58:37 +0600

    Scan and Print three number

---
+++ EnterThreeNumber.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    // Variable declare
+    int a,b,c;
+    // input 1st number
+    printf("Enter 1st number : a = \n");
+    scanf("%d",&a);
+
+    // input 2nd number
+    printf("Enter 2nd number: b =\n");
+    scanf("%d",&b);
+
+    // input 3rd number
+    printf("Enter 3rd number: c =\n");
+    scanf("%d",&c);
+
+    // print three number
+    printf("1st number : a = %d \n2nd number : b = %d \n3rd number : c = %d", a,b,c);
+    return 0;
+}
